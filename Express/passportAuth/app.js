927fce1cac028c2da224960bd6c24ffd624caefd
unknown <asifs.cse@gmail.com>
2023-05-18 00:43:06 +0530

    create all route

--- Express/passportAuth/app.js
+++ Express/passportAuth/app.js
@@ -8,8 +8,46 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+//base url
 app.get("/",(req, res)=>{
     res.render('index');
-})//
+});
+
+//register : get
+app.get("/register",(req, res)=>{
+    res.render('register');
+});
+
+//register : post
+app.post("/register",(req, res)=>{
+    try {
+        res.status(201).send("user is created");
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+//login: get
+app.get("/login",(req, res)=>{
+    res.render('login');
+});
+
+//login: post
+app.post("/login",(req, res)=>{
+    try {
+        res.status(201).send("user is login");
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+//profile protected route
+app.get("/profile",(req, res)=>{
+    res.render('profile');
+});
+
+//logout route
+app.get("/logout", (req, res)=>{
+    res.redirect("/");
+})
 
 module.exports = app;
